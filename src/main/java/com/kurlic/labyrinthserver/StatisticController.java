9a16a0e64354bdaff858f25d82ed3c20db7f0988
Alex Bogomol <quqveik.1@gmail.com>
2023-06-23 18:01:32 +0300

    Saved launches

--- src/main/java/com/kurlic/labyrinthserver/StatisticController.java
+++ src/main/java/com/kurlic/labyrinthserver/StatisticController.java
@@ -1,12 +1,24 @@
 package com.kurlic.labyrinthserver;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 @RestController
 public class StatisticController {
     int amountOfLaunches = 0;
+
+    public static String launchesPath = "savedVariable.txt";
+
+    public StatisticController() {
+        readLaunchesFromFile();
+    }
 
     public int getAmountOfLaunches()
     {
@@ -32,6 +44,26 @@ public class StatisticController {
         return getAmountOfLaunches();
     }
 
+    @Scheduled(fixedRate = 10000)
+    public void saveVariable() {
+        try (FileWriter writer = new FileWriter(launchesPath, false)) {
+            writer.write(String.valueOf(amountOfLaunches));
+            writer.flush();
+            System.out.println("Saved file" + amountOfLaunches);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private void readLaunchesFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(launchesPath))) {
+            String line = reader.readLine();
+            setAmountOfLaunches(Integer.parseInt(line));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+    }
 
 }
 
